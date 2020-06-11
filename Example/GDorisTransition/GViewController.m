3c516d71c3c8fbb19c81886c3be2cceefd6271cc
Ke G <giki.biu@gmail.com>
2020-06-11 15:33:12 +0800

    update GDorisTransition

--- Example/GDorisTransition/GViewController.m
+++ Example/GDorisTransition/GViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "GViewController.h"
-
-@interface GViewController ()
-
+@interface GViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, strong) UITableView * tableView;
+@property (nonatomic, strong) NSArray * datas;
 @end
 
 @implementation GViewController
@@ -17,13 +17,82 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"GDorisTransition";
+    
+    [self.view addSubview:({
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView.estimatedRowHeight = 0;
+        tableView.estimatedSectionFooterHeight = 0;
+        tableView.estimatedSectionHeaderHeight = 0;
+        _tableView = tableView;
+        tableView;
+    })];
+    
+    [self loadDatas];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidLayoutSubviews
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidLayoutSubviews];
+    self.tableView.frame = self.view.bounds;
 }
 
+- (void)loadDatas
+{
+    self.datas = @[@"头条评论转场"];
+}
+
+#pragma mark -- TableView DataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1    ;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.datas.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableViewCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"tableViewCell"];
+    }
+    cell.textLabel.text = [self.datas objectAtIndex:indexPath.row];;
+    cell.textLabel.textColor = [UIColor blackColor];
+    return cell;
+}
+
+#pragma mark -- TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 70;
+}
+
+//- (void)openPhotoPicker
+//{
+//
+//    GDorisPhotoPickerController * picker = [[GDorisPhotoPickerController alloc] init];
+//    UINavigationController *  nav = [[UINavigationController alloc] initWithRootViewController:picker];
+//    nav.modalPresentationStyle = UIModalPresentationCustom;
+//    GDorisSwipeGestureTransition * swipeTransition = [GDorisSwipeGestureTransition swipeGestureTransitionWithTarget:picker];
+//    swipeTransition.needsTopMask = YES;
+//    swipeTransition.containerOffset = 44;
+//    nav.transitioningDelegate = swipeTransition;
+//    nav.modalPresentationStyle = UIModalPresentationCustom;
+//    nav.modalPresentationCapturesStatusBarAppearance = YES;
+//    [self presentViewController:nav animated:YES completion:nil];
+//}
 @end
---
+++ Example/Pods/Target Support Files/Pods-GDorisTransition_Example/Pods-GDorisTransition_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GDorisTransition_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GDorisTransition_Example
+@end
